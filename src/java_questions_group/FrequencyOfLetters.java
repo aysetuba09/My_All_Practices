16eb9cd5cfe298109284310e1a81158766465ee7
aysetuba09 <atuba_90@hotmail.com>
2021-10-05 22:53:12 -0600

    made frequency letter class

--- src/java_questions_group/FrequencyOfLetters.java
+++ src/java_questions_group/FrequencyOfLetters.java
@@ -1,6 +1,10 @@
 package java_questions_group;
 
 public class FrequencyOfLetters {
+    /*Q1: String -- Frequency of Characters
+Write a return method that can find the frequency of characters
+Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2*/
+
     public static int frequencyOfLetter(String word, char c){
         int counter = 0;
         for (int i = 0; i < word.length(); i++) {
