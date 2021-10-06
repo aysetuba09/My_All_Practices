42457e7f4a4bd54c4bc15f72ce0e4fb9b98ab5df
aysetuba09 <atuba_90@hotmail.com>
2021-10-05 22:34:12 -0600

    made frequency letter class

---
+++ src/java_questions_group/FrequencyOfLetters.java
@@ -0,0 +1,23 @@
+package java_questions_group;
+
+public class FrequencyOfLetters {
+    public static int frequencyOfLetter(String word, char c){
+        int counter = 0;
+        for (int i = 0; i < word.length(); i++) {
+
+            if(word.charAt(i) == c)
+                counter++;
+
+        }
+        System.out.print(c);
+        return counter;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.print(frequencyOfLetter("aaabbbcccddd", 'a'));
+        System.out.print(frequencyOfLetter("aaabbbcccddd", 'b'));
+        System.out.print(frequencyOfLetter("aaabbbcccddd", 'c'));
+        System.out.print(frequencyOfLetter("aaabbbcccddd", 'd'));
+    }
+}
