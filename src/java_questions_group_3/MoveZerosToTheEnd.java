d9d57270d4d17e3479b93327026f7521909cf51d
aysetuba09 <atuba_90@hotmail.com>
2021-11-07 00:18:27 -0600

    update java questions group 3

--- src/java_questions_group_3/MoveZerosToTheEnd.java
+++
@@ -1,9 +0,0 @@
-package java_questions_group_3;
-/*
-Write a method that can move all the zeros to  last indexes of the array (Do Not Use Sort Method)
-
-
- */
-
-public class MoveZerosToTheEnd {
-}
